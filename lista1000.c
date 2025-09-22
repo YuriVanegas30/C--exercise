597bd7344c8ded6c438c1139ae1783bcc522bbcd
YuriVanegas30 <yavanegasr@academia.usbbog.edu.co>
2025-09-22 21:42:35 +0000

    Terminadooo

---
+++ lista1000.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    if (newNode == NULL) {
+        printf("Error: memoria no reservada\n");
+        exit(1);
+    }
+
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void printList(struct Node* head) {
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+void freeList(struct Node* head) {
+    struct Node* temp;
+    while (head != NULL) {
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+}
+
+int main() {
+    struct Node* head = NULL;
+    struct Node* tail = NULL;
+
+    for (int i = 0; i < 100000000; i++) {
+        struct Node* newNode = createNode(i);
+        if (head == NULL) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    printList(head);
+    freeList(head);
+
+    return 0;
+}
